b64c8f13df7a8d94e4a005afc1c52998a96f67eb
pengyunfei <pengyunfei@360.cn>
2024-01-11 15:07:38 +0800

    feat: 尝试pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), UnoCSS()],
+  plugins: [react(), UnoCSS(), VitePWA({
+    injectRegister: 'auto',
+    registerType: 'autoUpdate', devOptions: {
+      enabled: true
+    }
+  })],
 })
